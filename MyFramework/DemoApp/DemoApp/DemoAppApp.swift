4c5e01007cd2282fb984c2442e5501f3a18e4885
Anurag <38008425+anrgbhatt@users.noreply.github.com>
2024-12-18 18:22:17 +0530

    Add files via upload

---
+++ MyFramework/DemoApp/DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Anurag Bhatt on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
